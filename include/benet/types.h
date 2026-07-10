9110c58c81f7c544b2c4368219c6c1f6947a3b6a
Keunlas <126337231+keunlas@users.noreply.github.com>
2026-07-11 01:16:08 +0800

    feat(types): add Functor type alias for std::function<void()>

---
+++ include/benet/types.h
@@ -0,0 +1,17 @@
+// Distributed under the MIT License that can be found in the LICENSE file.
+// https://github.com/keunlas/benet
+//
+// Author: Keunlas <keunlaz at gmail dot com>
+
+#ifndef KEUNLAS_BENET_TYPES_H_
+#define KEUNLAS_BENET_TYPES_H_
+
+#include <functional>
+
+namespace benet {
+
+using Functor = std::function<void()>;
+
+}  // namespace benet
+
+#endif  // !KEUNLAS_BENET_TYPES_H_
